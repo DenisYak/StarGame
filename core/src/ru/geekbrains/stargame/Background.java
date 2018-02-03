a4bf06c15eb0e6884c7daa24db5ea855e4a48f5e
DenisYak <denis_yak@mail.ru>
2018-02-03 22:59:09 +0300

    add lesson3 code

---
+++ core/src/ru/geekbrains/stargame/Background.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.stargame;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import ru.geekbrains.stargame.engine.Sprite;
+import ru.geekbrains.stargame.engine.math.Rect;
+
+/**
+ * Created by Denis on 03.02.2018.
+ */
+
+public class Background extends Sprite{
+
+    public Background(TextureRegion region) {
+        super(region);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(worldBounds.getHeight());
+        pos.set(worldBounds.pos);
+    }
+}
--- core/src/ru/geekbrains/stargame/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/MenuScreen.java
@@ -6,9 +6,13 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Matrix3;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.stargame.engine.Base2DScreen;
+import ru.geekbrains.stargame.engine.Sprite;
+import ru.geekbrains.stargame.engine.math.Rect;
 
 /**
  * Created by Denis on 28.01.2018.
@@ -16,10 +20,14 @@ import ru.geekbrains.stargame.engine.Base2DScreen;
 
 public class MenuScreen extends Base2DScreen {
 
-    private SpriteBatch batch;
-    private Texture background;
+    private Texture backgroundTexture;
+//    private Texture logo;
+//    private Sprite logosprite;
+    private Background background;
+
     private Texture ship;
     private Vector2 shipLocation;
+    private Vector2 shipVelocity;
 
     public MenuScreen(Game game) {
         super(game);
@@ -29,12 +37,16 @@ public class MenuScreen extends Base2DScreen {
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
+//        logo = new Texture("badlogic.jpg");
+//        logosprite = new Sprite(new TextureRegion(logo));
+//        logosprite.setSize(1f, 1f);
 //        background = new Texture("space.png");
 //        background = new Texture("nebula.jpg");
-        background = new Texture("bg.png");
-        ship = new Texture("spaceship.png");
-        shipLocation = new Vector2(50, 50);
+        backgroundTexture = new Texture("bg.png");
+        background = new Background(new TextureRegion(backgroundTexture));
+//        ship = new Texture("spaceship.png");
+//        shipLocation = new Vector2(50, 50);
+//        shipVelocity = new Vector2(50, 50);
     }
 
     @Override
@@ -42,30 +54,33 @@ public class MenuScreen extends Base2DScreen {
         super.render(delta);
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
-            shipLocation.x = Gdx.input.getX() - ship.getWidth() / 2;
-            shipLocation.y = Gdx.graphics.getHeight() - Gdx.input.getY() - ship.getHeight() / 2;
-        }
+//        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+//            shipLocation.x = Gdx.input.getX() - ship.getWidth() / 2;
+//            shipLocation.y = Gdx.graphics.getHeight() - Gdx.input.getY() - ship.getHeight() / 2;
+//        }
         batch.begin();
-        batch.draw(background, 0, 0);
-        batch.draw(ship, shipLocation.x, shipLocation.y);
+//        batch.draw(background, 0, 0);
+//        batch.draw(logo, -0.5f, -0.5f, 1f, 1f);
+//        batch.draw(ship, shipLocation.x, shipLocation.y);
+//        logosprite.draw(batch);
+        background.draw(batch);
         batch.end();
+    }
+
+    @Override
+    protected void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        batch.dispose();
-        background.dispose();
+        backgroundTexture.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return super.touchDown(screenX, screenY, pointer, button);
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return super.touchUp(screenX, screenY, pointer, button);
+    protected void touchUp(Vector2 touch, int pointer) {
+        super.touchUp(touch, pointer);
     }
 }
